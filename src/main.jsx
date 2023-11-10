b4333cca7a77adbbba7f848041a6b7c235229861
tongjong <147476187+tongjong@users.noreply.github.com>
2023-11-10 14:34:56 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.jsx";
+import { jsxDEV } from "react/jsx-dev-runtime";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
