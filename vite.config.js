48eeafbccd83d85a3125aace0e44930f0eb1a77c
tongjong <147476187+tongjong@users.noreply.github.com>
2023-11-10 13:44:26 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 });
+
+import ReactRefresh from '@vitejs/plugin-react';
+
+export default {
+  plugins: [ReactRefresh()],
+};
+
